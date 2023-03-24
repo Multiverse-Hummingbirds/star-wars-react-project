2a6e257a5d43cd1007f6135d73fc39c844596f43
dantaayy <dantaayy@gmail.com>
2023-03-24 11:17:49 -0700

    added loading spinner to home and planets route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 import Home from './routes/Home';
 import Navigation from './components/Navigation';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,12 +12,15 @@ const starWarsAPI = `https://swapi.dev/api/people/`
 function App() {
 	// initialize state with people
 	const [peopleList, setPeopleList] = useState([]);
+	const [loading, setLoading] = useState(false)
 
 	//useEffect for fetching
 	useEffect(() => {
+		setLoading(true)
 		fetch(starWarsAPI)
 			.then((res) => res.json())
 			.then((data) => {
+				setLoading(false)
 				setPeopleList(data.results)
 			})
 			.catch((error) => {
@@ -27,7 +32,19 @@ function App() {
 		<BrowserRouter>
 			<Navigation />
 			<Routes>
-				<Route path="/" element={<Home peopleList={peopleList} />} />
+				<Route path="/" element={loading ? (
+					<Button variant='primary' disabled style={{ width: '10%', margin: '0 50rem' }}>
+						<Spinner
+							as="span"
+							animation="border"
+							size="sm"
+							role="status"
+							aria-hidden="true"
+						/>
+						Loading...
+					</Button>
+				) :
+					<Home peopleList={peopleList} />} />
 				<Route path="/planets" element={<Planets />} />
 			</Routes>
 		</BrowserRouter>
