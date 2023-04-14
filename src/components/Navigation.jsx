559e63216e6e0f334bc0c24c31dfdc4a3d37cc34
dantaayy <dantaayy@gmail.com>
2023-04-14 10:26:10 -0700

    updated the navbar logo for cleaner look

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
-import starWarsLogo from '../images/starWarsLogo.png'
+import starWarsTransparentLogo from '../images/Star-Wars-transparent-logo.png'
 import { Link } from 'react-router-dom'
 
 function Navigation() {
@@ -12,7 +12,7 @@ function Navigation() {
             <Container>
                 <Navbar.Brand>
                     <Nav.Link>
-                        <Link to='/'><Image src={starWarsLogo} width='95rem' className='me-2' /></Link>
+                        <Link to='/'><Image src={starWarsTransparentLogo} width='95rem' className='me-2' /></Link>
                     </Nav.Link>
                 </Navbar.Brand>
                 <Nav className='me-auto'>
